f9fb5b6609df4df10943716f6409c2aa1eb016c5
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-09-22 08:17:15 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clipnest-v1.8.0';
+const CACHE='clipnest-v1.8.1';
 const STATIC=['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-1024.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
